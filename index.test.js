e362d5d9b3d38209ddcb00513a9abc097bb3a31a
ding <playdingnow@gmail.com>
2017-12-19 11:06:06 +0800

    test: sparse mode

--- index.test.js
+++ index.test.js
@@ -22,11 +22,39 @@ const getModel = (mpagingPayload) => {
 const loadData = (model, count = testDocs.length) =>
   Promise.each(testDocs.slice(0, count), doc => model.create(doc));
 
+const rawMongooseCount = mongoose.Query.prototype.count;
+let counter = { call: false };
+mongoose.Query.prototype.count = function (...args) {
+  counter.call = true;
+  return rawMongooseCount.apply(this, args);
+};
+
+test.beforeEach(() => {
+  counter = { call: false };
+});
+
 test.after.always('clean up tmp collection', () =>
   Promise.fromNode(callback =>
     db.db.listCollections({ name: /^tmp/ }).toArray(callback))
     .map(collection => collection.name)
     .map(dropCollection));
+
+test('paging in sparse mode, query.count should not be called', (t) => {
+  const model = getModel();
+  const payload = {
+    sparse: true,
+  };
+  return loadData(model, 10)
+    .then(() => model.paging({}, payload))
+    .then(({ count, data }) => {
+      t.is(data.length, 10, 'default page size');
+      t.is(count, 10, 'docs count in db');
+      t.false(
+        counter.call,
+        'query.count should not be call when docs less then pagesize',
+      );
+    });
+});
 
 test('correctlly paging result', (t) => {
   const model = getModel();
